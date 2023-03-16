369b4ec2044993ab9b28cae5e8cffe808e556ef9
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-03-16 06:56:41 +0100

    deploy directory

---
+++ deploy/simple-mvc.sh
@@ -0,0 +1,17 @@
+if [ ! -d "../../projects/ " ]; then
+  mkdir ../projects
+fi
+if [ ! -d "../../projects/${1} " ]; then
+  cd "../../projects/"
+  git clone https://github.com/WildCodeSchool/${1}
+  cd -
+fi
+
+cd "../../projects/${1}"
+git pull origin main
+ENV_FILE=./.env
+if test -f "$ENV_FILE"; then
+    docker compose up -d --build --remove-orphans --force-recreate
+else
+    echo "file $ENV_FILE not found!"
+fi
--- deploy/symfony.sh
+++ deploy/symfony.sh
@@ -1,14 +1,15 @@
-if [ ! -d "../projects/ " ]; then
+if [ ! -d "../../projects/ " ]; then
   mkdir ../projects
 fi
-if [ ! -d "../projects/${1} " ]; then
-  cd "../projects/"
+if [ ! -d "../../projects/${1} " ]; then
+  cd "../../projects/"
   git clone https://github.com/WildCodeSchool/${1}
+  cd -
 fi
 
-cd "../projects/${1}"
+cd "../../projects/${1}"
 git pull origin main
-ENV_FILE=./.env
+ENV_FILE=./.env.local
 if test -f "$ENV_FILE"; then
     docker compose up -d --build --remove-orphans --force-recreate
 else
