285a1d49b32268cc4b3ce5025a7619fa67d04adb
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-07-11 07:38:56 +0200

    check if mailhog services exists before stop them on php deployment

--- deploy/php-projects.sh
+++ deploy/php-projects.sh
@@ -1,5 +1,7 @@
 #Stop all mailhog containers for better performance
+if [ "$(docker container ls -q --filter name=mailhog)" ]; then
 docker container stop $(docker container ls -q --filter name=mailhog)
+fi
 
 #Create projects directory if not exists
 if [ ! -d "../../projects/ " ]; then
@@ -29,4 +31,6 @@ echo $VARS | jq 'to_entries[] | "\(.key)=\(.value)"' | sed 's/"//g' > ../envs/.e
 GITHUB_REPOSITORY_NAME=${1} PROJECT_NAME=${2} DB_NAME=`echo "${1}" | sed 's/\-/\_/g'` docker compose --env-file ../../traefik/data/.env up -d --build --remove-orphans --force-recreate
 
 #Restart mailhog containers
+if [ "$(docker container ls -qa --filter name=mailhog)" ]; then
 docker container restart $(docker container ls -qa --filter name=mailhog)
+fi
